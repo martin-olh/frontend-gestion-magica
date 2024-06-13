f90625b71ef6b6a55f90b03a475c452e2cf7cd41
martin-olh <momosmirg@gmail.com>
2024-06-12 21:21:46 -0300

    fix(routes): test arreglo rutas

--- src/App.jsx
+++ src/App.jsx
@@ -27,12 +27,12 @@ export const App = () => {
         <Route path="/" element={<Dashboard />}>
           <Route index element={<Home />}></Route>
           <Route path="/home" element={<Home />}></Route>
-          <Route path="/usuarios/agregar" element={<ProtectedRoute component={AgregarUsuario} allowedRoles={['Administrador']} />}></Route>
-          <Route path="/usuarios/listado" element={<ProtectedRoute component={ListadoUsuarios} allowedRoles={['Administrador']} />}></Route>
-          <Route path="/usuarios/editar/:id" element={<ProtectedRoute component={EditarUsuario} allowedRoles={['Administrador']} />}></Route>
-          <Route path="/usuarios/settings/:id" element={<ProtectedRoute component={SettingsUsuario} allowedRoles={['Administrador', 'Maestro', 'Coordinador']} />}></Route>
-          <Route path="/alumnos/agregar" element={<ProtectedRoute component={AgregarAlumno} allowedRoles={['Administrador', 'Maestro', 'Coordinador']} />}></Route>
-          <Route path="/cursos/agregar" element={<ProtectedRoute component={AgregarCurso} allowedRoles={['Administrador']} />}></Route>
+          <Route path="/usuarios/agregar" element={<AgregarUsuario />}></Route>
+          <Route path="/usuarios/listado" element={<ListadoUsuarios />}></Route>
+          <Route path="/usuarios/editar/:id" element={<EditarUsuario />}></Route>
+          <Route path="/usuarios/settings/:id" element={<SettingsUsuario />}></Route>
+          <Route path="/alumnos/agregar" element={<AgregarAlumno />}></Route>
+          <Route path="/cursos/agregar" element={<AgregarCurso />}></Route>
         </Route>
         <Route path="*" element={<Navigate replace to={"/"}></Navigate>} />
 
@@ -40,3 +40,10 @@ export const App = () => {
     </BrowserRouter >
   )
 }
+
+// <Route path="/usuarios/agregar" element={<ProtectedRoute component={AgregarUsuario} allowedRoles={['Administrador']} />}></Route>
+//           <Route path="/usuarios/listado" element={<ProtectedRoute component={ListadoUsuarios} allowedRoles={['Administrador']} />}></Route>
+//           <Route path="/usuarios/editar/:id" element={<ProtectedRoute component={EditarUsuario} allowedRoles={['Administrador']} />}></Route>
+//           <Route path="/usuarios/settings/:id" element={<ProtectedRoute component={SettingsUsuario} allowedRoles={['Administrador', 'Maestro', 'Coordinador']} />}></Route>
+//           <Route path="/alumnos/agregar" element={<ProtectedRoute component={AgregarAlumno} allowedRoles={['Administrador', 'Maestro', 'Coordinador']} />}></Route>
+//           <Route path="/cursos/agregar" element={<ProtectedRoute component={AgregarCurso} allowedRoles={['Administrador']} />}></Route>
