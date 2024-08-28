b8c8785ea9f356562572d6ed1a3049813d7ea226
martin-olh <momosmirg@gmail.com>
2024-08-28 00:50:57 -0300

    Fix display de la pagina.

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -7,10 +7,10 @@ export const Menu = () => {
     const tipoUsuario = sessionStorage.getItem('tipoUsuario')
 
     return (
-        <>
+        <div className="sticky-menu">
             {tipoUsuario === "Administrador" && <MenuAdmin></MenuAdmin>}
             {tipoUsuario === "Coordinador" && <MenuCoordinador></MenuCoordinador>}
             {tipoUsuario === "Maestro" && <MenuMaestro></MenuMaestro>}
-        </>
+        </div>
     )
 }
