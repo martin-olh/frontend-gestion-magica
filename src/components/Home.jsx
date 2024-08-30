4dddd4b169b0ee1d1e5fa0587dc6f730a28c8d70
martin-olh <momosmirg@gmail.com>
2024-08-30 00:00:29 -0300

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -14,7 +14,7 @@ export const Home = () => {
                 <Col xs={6}>
                     <Card className='welcome'>
                         <Card.Body >
-                            <h3 className='mb-3'>Bienvenido {nombre}!</h3>
+                            <h3 className='mb-3'>Bienvenido/a {nombre}!</h3>
                             <img className='mb-3' src={imgHome} alt="" />
                             <p> Seleccionar una opción del menú para comenzar</p>
 
